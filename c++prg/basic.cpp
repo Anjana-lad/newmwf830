b2a8b5200c772a1d85d53a25eebadefac58b4f14
Anjana-lad <iantanjanalad@gmail.com>
2024-09-10 16:06:14 +0530

    oop

---
+++ c++prg/basic.cpp
@@ -0,0 +1,11 @@
+// basic structure of program.
+#include <iostream>
+using namespace std;
+int main(){
+    std::cout<<"Hello world"<<std::endl<<"Welcome to  OOPs concepts."<<std::endl;
+    int num1;
+    cout<<"Enter number:";
+    cin>>num1;
+    cout<<"Num1="<<num1<<endl;
+    return 0;
+}
